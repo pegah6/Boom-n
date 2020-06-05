56a5ab9dbfd0cc2023f8b702122ef6d1985ac7e7
pegah6 <62961787+pegah6@users.noreply.github.com>
2020-06-05 14:42:49 +0430

    Create OrderDetail.cs

---
+++ BoomAspCore/Models/OrderDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoomAspCore.Models
+{
+    public class OrderDetail
+    {
+            public int ID { get; set; }
+            public int OrderId { get; set; }
+            public int ProductId { get; set; }
+            public int quantity { get; set; }
+
+            [ForeignKey("OrderId")]
+            public virtual Order Order { get; set; }
+            [ForeignKey("ProductId")]
+            public virtual Product Product { get; set; }
+        
+    }
+}
