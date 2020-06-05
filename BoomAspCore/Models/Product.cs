da11b997b653165b793740647017b03a76b3224e
pegah6 <62961787+pegah6@users.noreply.github.com>
2020-06-05 14:43:08 +0430

    Create Product.cs

---
+++ BoomAspCore/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoomAspCore.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string  Description { get; set; }
+
+        public int CategoryID { get; set; }
+
+        // Product & Category 
+        [ForeignKey("CategoryID")]
+        public virtual  Category Category { get; set; }
+
+        // Product & Comment
+        public virtual ICollection<Comment> Comment { get; set; }
+    }
+}
