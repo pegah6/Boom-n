a7b35997a4d2692068b8c7ee8060a5be79767a13
pegah6 <62961787+pegah6@users.noreply.github.com>
2020-06-05 14:42:19 +0430

    Create Comment.cs

---
+++ BoomAspCore/Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoomAspCore.Models
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        public string username { get; set; }
+        public string name { get; set; }
+        public string comment_text { get; set; }
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+    }
+}
